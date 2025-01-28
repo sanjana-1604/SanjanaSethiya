a822c31199129d370b8511956c370d22fb2c1379
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2025-01-28 19:03:55 +0530

    generic code for list

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -8,6 +8,24 @@ const projects=[
     link:'https://doc-mini-app-psi.vercel.app/'
   },
   {
+    name: 'Doc-Mini-App',
+    detail:'Application made using',
+    image: 'Doc-Mini-App.png',
+    link:'https://doc-mini-app-psi.vercel.app/'
+
+  },
+  {
+    name: 'Doc-Mini-App',
+    detail:'Application made using',
+    image: 'Doc-Mini-App.png',
+    link:'https://doc-mini-app-psi.vercel.app/'
+
+  },
+  {
+    name: 'Doc-Mini-App',
+    detail:'Application made using',
+    image: 'Doc-Mini-App.png',
+    link:'https://doc-mini-app-psi.vercel.app/'
 
   }
 ]
@@ -19,43 +37,26 @@ function Work() {
    <h3>Recent Works</h3>
    <h1 className=' text-[50px] font-Dm-serif leading-relaxed'>Here are some of my favourite projetcs 
     I have done lately. Feel free to check them out.</h1>
-    <table className="w-full border-collapse">
-      <tr className='flex justify-around'> 
-        <td>
-          <a href='https://doc-mini-app-psi.vercel.app/' target="_blank" rel="noopener noreferrer" className='cursor-wait flex border-slate-900 border-t-[1px]'>
+
+<div className='grid grid-cols-2 w-full border-collapse'>
+  {projects.map((item)=>{
+    return <div >
+<a href={item.link} target="_blank" rel="noopener noreferrer" className='flex border-slate-900 border-t-[1px]'>
          
-         <img className='w-[150px] h-[150px] p-2 mr-2 hover:opacity-70' src='./public/Doc-Mini-App.png' />
+         <img className='w-[150px] h-[150px] p-2 mr-2 hover:opacity-70' src={`./public/${item.image}`} />
          <div className='m-4'>
-         <h5 className='font-Dm-serif'>Doc-Mini-App</h5>
-         <p className=''>Application made using </p>
+         <h5 className='font-Dm-serif'>{item.name}</h5>
+         <p className=''>{item.detail} </p>
          </div>
-        </a>
-         </td>
-         <td>
-          <a href='https://doc-mini-app-psi.vercel.app/' target="_blank" rel="noopener noreferrer" className='flex border-slate-900 border-t-[1px]'>
-         
-         <img className='w-[150px] h-[150px] p-2 mr-2' src='./public/Doc-Mini-App.png' />
-         <div className='m-4'>
-         <h5 className='font-Dm-serif'>Doc-Mini-App</h5>
-         <p className=''>Application made using </p>
-         </div>
-        </a>
-         </td>
-         
-        {/* <td><a href='https://25-reactjs-projects.vercel.app/' target='blank'>
-         
-         <img className='w-[150px] h-[150px]' src='./public/React-projects.png' />
-         <div>
-         <h5>Doc-Mini-App</h5>
-         <p>Application made using </p>
-         </div>
-     
-     </a> </td>
-     */}
-            
-      </tr>
-      
-    </table>
+         </a>
+
+    </div>
+
+
+
+  })}
+  
+</div>
     </div>
    </>
   )
